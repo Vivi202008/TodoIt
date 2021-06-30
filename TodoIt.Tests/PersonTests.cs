0d0010904c0d31ac49787e811d5cb3531bbe9e28
Wei <Wei@DESKTOP-530AD3K>
2021-06-30 15:30:58 +0200

    Add project files.

---
+++ TodoIt.Tests/PersonTests.cs
@@ -0,0 +1,59 @@
+using System;
+using Xunit;
+using TodoIt.Model;
+
+namespace TodoIt.Tests
+{
+    public class PersonTests
+    {
+        [Fact]
+        public void IdCounterWorks()
+        {
+            //Arrange
+            string firstName = "Kent";
+            string lastName = "Svensson";
+
+            //Act
+            int before = Person.Counter;
+            new Person(firstName, lastName);//create person to make counter count up.
+            int result = Person.Counter;
+
+            //Assert
+            Assert.True(before < result);
+        }
+
+        [Fact]
+        public void PersonIdWorks()
+        {
+            //Arrange
+            string firstName = "Kent";
+            string lastName = "Svensson";
+            string firstName2 = "Test";
+            string lastName2 = "Testsson";
+
+            //Act
+            Person person1 = new Person(firstName, lastName);
+            Person person2 = new Person(firstName2, lastName2);
+
+            //Assert
+            Assert.NotEqual(person1.personId, person2.personId);
+        }
+
+        [Fact]
+        public void DetailsContainsCorrectInfo()
+        {
+            //Arrange
+            string firstName = "Kent";
+            string lastName = "Svensson";
+
+            Person testPerson = new Person(firstName, lastName);
+
+            //Act
+            string result = testPerson.Details();
+
+            //Assert
+            Assert.Contains(firstName, result);
+            Assert.Contains(lastName, result);
+        }
+    }
+}
---
+++ TodoIt/Model/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Model
+
+{
+    public class Person
+    {
+        static int idCounter = 0;
+        public static int Counter { get { return idCounter; } }
+
+        public readonly int personId;
+        string firstName;
+        string lastName;//default value stats with is null.
+       
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Empty or only whitespace is not allowed.");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Empty or only whitespace is not allowed.");
+                }
+
+                lastName = value;
+            }
+        }
+        public string FullName { get { return firstName + ' ' + lastName; } }
+       
+        public Person(string firstName, string lastName)
+        {
+            personId = ++idCounter;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+         public string Details()
+        {
+            return $"Name: {FullName}\npersonId: {personId}";
+        }
+    }
+}
