88312a97a62fa951838358ba636990707f6bc230
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 09:11:58 +0200

    omskrivning av Details i Person.cs till att använda '${...'

--- TodoIt.Tests/PersonTests.cs
+++ TodoIt.Tests/PersonTests.cs
@@ -6,55 +6,22 @@ namespace TodoIt.Tests
 {
     public class PersonTests
     {
-        [Fact]
-        public void IdCounterWorks()
-        {
-            //Arrange
-            string firstName = "Kent";
-            string lastName = "Svensson";
+	[Fact]
+	public void IdCounterWorks()
+	{
+	    //Arrange
+	    string firstName = "Kent";
+	    string lastName = "Svensson";
 
-            //Act
-            int before = Person.Counter;
-            new Person(firstName, lastName);//create person to make counter count up.
-            int result = Person.Counter;
+	    //Act
+	    int before = Person.Counter;
+	    new Person(firstName, lastName);//create person to make counter count up.
+	    int result = Person.Counter;
 
-            //Assert
-            Assert.True(before < result);
-        }
+	    //Assert
+	    Assert.True(before < result);
+	}
 
-        [Fact]
-        public void PersonIdWorks()
-        {
-            //Arrange
-            string firstName = "Kent";
-            string lastName = "Svensson";
-            string firstName2 = "Test";
-            string lastName2 = "Testsson";
-
-            //Act
-            Person person1 = new Person(firstName, lastName);
-            Person person2 = new Person(firstName2, lastName2);
-
-            //Assert
-            Assert.NotEqual(person1.personId, person2.personId);
-        }
-
-        [Fact]
-        public void DetailsContainsCorrectInfo()
-        {
-            //Arrange
-            string firstName = "Kent";
-            string lastName = "Svensson";
-
-            Person testPerson = new Person(firstName, lastName);
-
-            //Act
-            string result = testPerson.Details();
-
-            //Assert
-            Assert.Contains(firstName, result);
-            Assert.Contains(lastName, result);
-        }
 	[Fact]
 	public void IdCounterWorks2()
 	{
@@ -67,5 +34,38 @@ namespace TodoIt.Tests
 	    Assert.True(before < Person.Counter);
 	}
 
+	[Fact]
+	public void PersonIdWorks()
+	{
+	    //Arrange
+	    string firstName = "Kent";
+	    string lastName = "Svensson";
+	    string firstName2 = "Test";
+	    string lastName2 = "Testsson";
+
+	    //Act
+	    Person person1 = new Person(firstName, lastName);
+	    Person person2 = new Person(firstName2, lastName2);
+
+	    //Assert
+	    Assert.NotEqual(person1.personId, person2.personId);
+	}
+
+	[Fact]
+	public void DetailsContainsCorrectInfo()
+	{
+	    //Arrange
+	    string firstName = "Kent";
+	    string lastName = "Svensson";
+
+	    Person testPerson = new Person(firstName, lastName);
+
+	    //Act
+	    string result = testPerson.Details();
+
+	    //Assert
+	    Assert.Contains(firstName, result);
+	    Assert.Contains(lastName, result);
+	}
     }
 }
