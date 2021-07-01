a1cb8b9f768cf07d574d308f1d0105957a93026e
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 17:31:06 +0200

    Sekvenserklasserna

--- TodoIt/Data/PersonSequencer.cs
+++ TodoIt/Data/PersonSequencer.cs
@@ -4,20 +4,18 @@ using System.Text;
 
 namespace TodoIt.Data
 {
-    class PersonSequencer
+    public class PersonSequencer
     {
-        private static int personId;
+	private static int personId=0;
 
-        public static int NextPersonId()
-        {
-            return ++personId;
-        }
+	public static int nextPersonId()
+	{
+	    return ++personId;
+	}
 
-        public static void Reset()
-        {
-            personId = 0;
-
-        }
-
+	public static void reset()
+	{
+	    personId=0;
+	}
     }
 }
--- TodoIt/Data/TodoSequencer.cs
+++ TodoIt/Data/TodoSequencer.cs
@@ -4,21 +4,18 @@ using System.Text;
 
 namespace TodoIt.Data
 {
-    class TodoSequencer
+    public class TodoSequencer
     {
+	private static int todoId=0;
 
-        private static int todoId;
+	public static int nextTodoID()
+	{
+	    return ++todoId;
+	}
 
-        public static int NextTodoId()
-        {
-            return ++todoId;
-        }
-
-        public static void Reset()
-        {
-            todoId = 0;
-        }
-
+	public static void reset()
+	{
+	    todoId=0;
+	}
     }
 }
-
