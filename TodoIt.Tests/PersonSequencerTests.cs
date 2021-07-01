0c495659c07c9317c1f435c61738008c593edca2
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 18:24:51 +0200

    bygger ut testen av sekvensklasserna

--- TodoIt.Tests/PersonSequencerTests.cs
+++ TodoIt.Tests/PersonSequencerTests.cs
@@ -7,7 +7,9 @@ namespace TodoIt.Tests
     public class PersonSequencerTests
     {
 	//
-	// kontrollera att id från sekvensklassen börjar på 1
+	// kontrollera att PersonSequencer när den startar alltid börjar på 1
+	//
+	// Todo klassens assignee är like med 0 när assignee ännu inte är tilldelad
 	//
 	[Fact]
 	public void IdCounterWorks1()
@@ -34,9 +36,14 @@ namespace TodoIt.Tests
 	    //Act
 	    int firstId  = PersonSequencer.nextPersonId();
 	    int secondId = PersonSequencer.nextPersonId();
+	    int thirdId = PersonSequencer.nextPersonId();
 
 	    //Assert
+	    Assert.True(firstId != secondId);
+	    Assert.True(firstId != thirdId);
+	    Assert.True(secondId != thirdId);
 	    Assert.True(firstId < secondId);
+	    Assert.True(secondId < thirdId);
 	}
     }
 }
