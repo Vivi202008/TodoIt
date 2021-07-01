cd779d79d64fc3279a0f55ea7d9a08b4a6ce0562
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 11:29:45 +0200

    lite fler operationer i Todo klassen

--- TodoIt/Model/Todo.cs
+++ TodoIt/Model/Todo.cs
@@ -6,8 +6,39 @@ namespace TodoIt.Model
 {
     class Todo
     {
-        private readonly int todoId;
-        string description;
-        bool done;
+	private readonly int todoId;
+	string description;
+	Person assignee;
+	bool done;
+
+	public Todo( int id, string description)
+	{
+	    if (string.IsNullOrEmpty(description))
+	    {
+		throw new ArgumentException("Empty or only whitespace is not allowed.");
+	    }
+	    todiId = id;
+	    this.description = description;
+	}
+
+	public string Description {
+	    get { return description }
+	    set
+	    {
+		if (string.IsNullOrEmpty(value))
+		{
+		    throw new ArgumentException("Empty or only whitespace is not allowed.");
+		}
+		this.description = value;
+	    }
+	}
+
+	public Person Assignee {
+	    get { return assignee }
+	    set
+	    {
+		this.assignee = value;
+	    }
+	}
     }
 }
