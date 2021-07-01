82a51b14cf9e0d858c3ec40e90193b8db594e1c0
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 18:12:42 +0200

    ensning

--- TodoIt/Model/Person.cs
+++ TodoIt/Model/Person.cs
@@ -10,7 +10,7 @@ namespace TodoIt.Model
 	static int idCounter = 0;
 	public static int Counter { get { return idCounter; } }
 
-	public readonly int personId;
+	private readonly int personId;
 	public int PersonId { get { return personId; } }
 
 	string firstName;
