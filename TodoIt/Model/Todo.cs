f41f9b06788a0e4b061d2315277c3f2fbae61030
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 14:07:33 +0200

    Todo listan

--- TodoIt/Model/Todo.cs
+++ TodoIt/Model/Todo.cs
@@ -6,58 +6,59 @@ namespace TodoIt.Model
     class Todo
     {
 
-        static int idCount = 0;
-        public static int Counter { get { return idCount; } }
-        private readonly int todoId;
+	static int idCount = 0;
+	public static int Counter { get { return idCount; } }
+	private readonly int todoId;
 
-        private string description;
-        private bool done;
-        private static string firstName;
-        private static string lastName;
-        private static Person assignee = new Person(firstName, lastName);
+	private string description;
+	private bool done;
+	// private static string firstName;
+	// private static string lastName;
+	// private static Person assignee = new Person(firstName, lastName);
+	private static Person assignee;
 
-        public Person Assignee
-        {
-            get { return assignee; }
-            set
-            {
-                assignee = value;
-            }
-        }
-        public string Description
-        {
-            get { return description; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Empty or only whitespace is not allowed.");
-                }
+	public Person Assignee
+	{
+	    get { return assignee; }
+	    set
+	    {
+		assignee = value;
+	    }
+	}
+	public string Description
+	{
+	    get { return description; }
+	    set
+	    {
+		if (string.IsNullOrEmpty(value))
+		{
+		    throw new ArgumentException("Empty or only whitespace is not allowed.");
+		}
 
-                description = value;
-            }
-        }
+		description = value;
+	    }
+	}
 
-        public bool Done
-        {
-            get { return done; }
-            set
-            {
-                done = value;
-            }
-        }
+	public bool Done
+	{
+	    get { return done; }
+	    set
+	    {
+		done = value;
+	    }
+	}
 
-        public Todo(string description, bool done, Person assignee)
-        {
-            todoId = ++idCount;
-            Description = description;
-            Done = done;
-            Assignee = assignee;
-        }
+	public Todo(string description, Person nominatedAssignee)
+	{
+	    todoId = ++idCount;
+	    Description = description;
+	    Done = false;
+	    Assignee = nominatedAssignee;
+	}
 
-        public string Details()
-        {
-            return $"todoId: {todoId}\nDescription: {description}\nDone?:{done}\nAssignee:{assignee}";
-        }
+	public string Details()
+	{
+	    return $"todoId: {todoId}\nDescription: {description}\nDone?:{done}\nAssignee:{assignee}";
+	}
     }
 }
