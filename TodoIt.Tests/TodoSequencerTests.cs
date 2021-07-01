b53e65faa9ea7a51760f4b0900654d6bc71e505d
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-01 18:05:34 +0200

    test sekvensklasserna

---
+++ TodoIt.Tests/TodoSequencerTests.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+using TodoIt.Data;
+
+namespace TodoIt.Tests
+{
+    public class TodoSequencerTests
+    {
+	//
+	// kontrollera att id från sekvensklassen börjar på 1
+	//
+	[Fact]
+	public void IdCounterWorks1()
+	{
+	    //Arrange
+	    TodoSequencer.reset();
+
+	    //Act
+	    int firstId = TodoSequencer.nextTodoID();
+
+	    //Assert
+	    Assert.True( firstId == 1);
+	}
+
+	//
+	// kontrollera att id från sekvensklassen ökar
+	//
+	[Fact]
+	public void IdCounterWorks2()
+	{
+	    //Arrange
+	    TodoSequencer.reset();
+
+	    //Act
+	    int firstId  = TodoSequencer.nextTodoID();
+	    int secondId = TodoSequencer.nextTodoID();
+
+	    //Assert
+	    Assert.True(firstId < secondId);
+	}
+    }
+}
