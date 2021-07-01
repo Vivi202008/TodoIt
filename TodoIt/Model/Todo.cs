fefc6949fedf7814dac26072aa7f0b1eb4426471
Wei Cao <viviancao0728@gmail.com>
2021-07-02 00:14:23 +0200

    Write People.cs and PeopleTest.cs.
    There is a fel :  PeopleTests.AddPersons() line 39

--- TodoIt/Model/Todo.cs
+++ TodoIt/Model/Todo.cs
@@ -4,63 +4,63 @@ namespace TodoIt.Model
 {
     public class Todo
     {
-	static int idCount = 0;
-	public static int Counter { get { return idCount; } }
-	private readonly int todoId;
-	public int TodoId { get { return todoId; } }
+        static int idCount = 0;
+        public static int Counter { get { return idCount; } }
+        private readonly int todoId;
+        public int TodoId { get { return todoId; } }
 
-	private string description;
-	private bool done;
-	private static Person assignee;
+        private string description;
+        private bool done;
+        private static Person assignee;
 
-	public Person Assignee
-	{
-	    get { return assignee; }
-	    set
-	    {
-		assignee = value;
-	    }
-	}
-	public string Description
-	{
-	    get { return description; }
-	    set
-	    {
-		if (string.IsNullOrEmpty(value))
-		{
-		    throw new ArgumentException("Empty or only whitespace is not allowed.");
-		}
-		description = value;
-	    }
-	}
+        public Person Assignee
+        {
+            get { return assignee; }
+            set
+            {
+                assignee = value;
+            }
+        }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Empty or only whitespace is not allowed.");
+                }
+                description = value;
+            }
+        }
 
-	public bool Done
-	{
-	    get { return done; }
-	    set
-	    {
-		done = value;
-	    }
-	}
+        public bool Done
+        {
+            get { return done; }
+            set
+            {
+                done = value;
+            }
+        }
 
-	public Todo(string description, Person nominatedAssignee)
-	{
-	    todoId = ++idCount;
-	    Description = description;
-	    Done = false;
-	    Assignee = nominatedAssignee;
-	}
+        public Todo(string description, Person nominatedAssignee)
+        {
+            todoId = ++idCount;
+            Description = description;
+            Done = false;
+            Assignee = nominatedAssignee;
+        }
 
-	public Todo(int id, string description)
-	{
-	    todoId = id;
-	    Description = description;
-	    Done = false;
-	}
+        public Todo(int id, string description)
+        {
+            todoId = id;
+            Description = description;
+            Done = false;
+        }
 
-	public string Details()
-	{
-	    return $"todoId: {todoId}\nDescription: {description}\nDone?:{done}\nAssignee:{assignee}";
-	}
+        public string Details()
+        {
+            return $"todoId: {todoId}\nDescription: {description}\nDone?:{done}\nAssignee:{assignee}";
+        }
     }
 }
