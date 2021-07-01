da9f52e0fc9771c1571d7d54b50e8e05d90f12a8
Wei Cao <viviancao0728@gmail.com>
2021-07-01 09:47:37 +0200

    Creat Todo.cs and TodoTests.cs

---
+++ TodoIt.Tests/TodoTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using TodoIt.Model;
+
+namespace TodoIt.Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+    }
+}
