f04dd7c7052c8664991611ba20f525dc5629a0ec
Wei Cao <viviancao0728@gmail.com>
2021-07-01 15:12:30 +0200

    New codes to TodoTests. a fel  about todoId.

--- TodoIt.Tests/TodoTests.cs
+++ TodoIt.Tests/TodoTests.cs
@@ -2,48 +2,71 @@ using System;
 using Xunit;
 using TodoIt.Model;
 
+
 namespace TodoIt.Tests
 {
     public class TodoTests
     {
         [Fact]
-        public void Test1()
+        public void IdCounterTest()
         {
             //Arrange
-
+            string firstName = "Billy";
+            string lastName = "Svensson";
+            string description = "The work is a game---Hangman";
+            Person assignee = new Person(firstName, lastName);
 
             //Act
-
+            int before = Todo.Counter;
+            new Todo(description, assignee);//create person to make counter count up.
+            int result = Todo.Counter;
 
             //Assert
+            Assert.True(before < result);
 
         }
 
         [Fact]
-        public void Test2()
+        public void todoIdTest()
         {
-
+            //Arrange
+            string firstName1 = "Kent";
+            string lastName1 = "Svensson";
+            string firstName2 = "Billy";
+            string lastName2 = "Eriksson";
+            Person assignee1 = new Person(firstName1, lastName1);
+            Person assignee2 = new Person(firstName2, lastName2);
+            string description1 = "The work is a calculator";
+            string description2 = "The work a game---Hangman";
 
             //Act
-
+            Todo todo1 = new Todo(description1, assignee1);
+            Todo todo2 = new Todo(description2, assignee2);
 
             //Assert
-
+            Assert.NotEqual(todo1.todoId, todo2.todoId);
         }
 
         [Fact]
-        public void Test3()
+        public void DetailsContainsCorrectInfo()
         {
             //Arrange
+            string firstName = "Kent";
+            string lastName = "Svensson";
+            string description = "The work is a game---Hangman";
+            Person assignee = new Person(firstName, lastName);
 
 
             //Act
+            Todo todo1 = new Todo(description, assignee);
 
+            var result = todo1.Details();
 
             //Assert
-
+            Assert.Contains(description, result);
+            Assert.NotNull(description);
         }
-        
+
 
     }
 }
