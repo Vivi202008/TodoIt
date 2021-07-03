c556b1ce93ee2d6ae1f70b3c6f5193540612f7e2
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-03 23:34:52 +0200

    PersonSequencer.reset() i testrutinen

--- TodoIt.Tests/PeopleTests.cs
+++ TodoIt.Tests/PeopleTests.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,102 +13,108 @@ namespace TodoIt.Tests
     public class PeopleTests
     {
 
-        [Fact]
-        public void AddPersons()
-        {
+	[Fact]
+	public void AddPersons()
+	{
 
-            // Arrange
-            string expectedFirstName = "Erik";
-            string expectedLastName = "Eriksson";
+	    // Arrange
+	    string expectedFirstName = "Erik";
+	    string expectedLastName = "Eriksson";
 
-            string expectedFirstName1 = "Jonas";
-            string expectedLastName1 = "Jonasson";
+	    string expectedFirstName1 = "Jonas";
+	    string expectedLastName1 = "Jonasson";
 
-            // Act
-            People actualPeople = new People();
-            Person actualPerson = actualPeople.AddPerson(expectedFirstName, expectedLastName);
-            Person actualPerson1 = actualPeople.AddPerson(expectedFirstName1, expectedLastName1);
-            Person[] testPersonArray = actualPeople.FindAll();
+	    // nolla PersonSequencer för att få förutsägelsebara personId
+	    PersonSequencer.reset();
 
-            // Assert
-            Assert.Equal(expectedFirstName, actualPerson.FirstName);
-            Assert.Equal(expectedLastName, actualPerson.LastName);
-            Assert.Equal(expectedFirstName1, actualPerson1.FirstName);
-            Assert.Equal(expectedLastName1, actualPerson1.LastName);
-            Assert.Contains(actualPerson, testPersonArray);
-            Assert.Contains(actualPerson1, testPersonArray);
+	    // Act
+	    People actualPeople = new People();
+	    Person actualPerson = actualPeople.AddPerson(expectedFirstName, expectedLastName);
+	    Person actualPerson1 = actualPeople.AddPerson(expectedFirstName1, expectedLastName1);
+	    Person[] testPersonArray = actualPeople.FindAll();
 
-            Assert.NotEqual(actualPerson.PersonId, actualPerson1.PersonId);
-            Assert.True(actualPerson.PersonId == 1);
-            Assert.True(actualPerson1.PersonId == 2);
+	    // Assert
+	    Assert.Equal(expectedFirstName, actualPerson.FirstName);
+	    Assert.Equal(expectedLastName, actualPerson.LastName);
+	    Assert.Equal(expectedFirstName1, actualPerson1.FirstName);
+	    Assert.Equal(expectedLastName1, actualPerson1.LastName);
+	    Assert.Contains(actualPerson, testPersonArray);
+	    Assert.Contains(actualPerson1, testPersonArray);
 
-            Assert.Contains(actualPeople.FindById(1).FirstName, "Erik");
-            Assert.Contains(actualPeople.FindById(2).FirstName, "Jonas");
+	    Assert.NotEqual(actualPerson.PersonId, actualPerson1.PersonId);
+	    Assert.True(actualPerson.PersonId == 1);
+	    Assert.True(actualPerson1.PersonId == 2);
 
-        }
+	    Assert.Contains(actualPeople.FindById(1).FirstName, "Erik");
+	    Assert.Contains(actualPeople.FindById(2).FirstName, "Jonas");
 
-        [Fact]
-        public void RemovePersons()
-        {
+	}
 
-            // Arrange
-            string expectedFirstName = "Erik";
-            string expectedLastName = "Eriksson";
+	[Fact]
+	public void RemovePersons()
+	{
 
-            string expectedFirstName1 = "Jonas";
-            string expectedLastName1 = "Jonasson";
+	    // Arrange
+	    string expectedFirstName = "Erik";
+	    string expectedLastName = "Eriksson";
 
-            string expectedFirstName2 = "Helen";
-            string expectedLastName2 = "Eriksson";
+	    string expectedFirstName1 = "Jonas";
+	    string expectedLastName1 = "Jonasson";
 
-            string expectedFirstName3 = "Eva";
-            string expectedLastName3 = "Jonasson";
+	    string expectedFirstName2 = "Helen";
+	    string expectedLastName2 = "Eriksson";
 
-            string expectedFirstName4 = "Ulf";
-            string expectedLastName4 = "Eriksson";
+	    string expectedFirstName3 = "Eva";
+	    string expectedLastName3 = "Jonasson";
 
-            string expectedFirstName5 = "Johan";
-            string expectedLastName5 = "Jonasson";
+	    string expectedFirstName4 = "Ulf";
+	    string expectedLastName4 = "Eriksson";
 
-            // Act
-            // PersonSequencer.reset();
-            People actualPeople = new People();
-            Person actualPerson = actualPeople.AddPerson(expectedFirstName, expectedLastName);
-            Person actualPerson1 = actualPeople.AddPerson(expectedFirstName1, expectedLastName1);
-            Person actualPerson2 = actualPeople.AddPerson(expectedFirstName2, expectedLastName2);
-            Person actualPerson3 = actualPeople.AddPerson(expectedFirstName3, expectedLastName3);
-            Person actualPerson4 = actualPeople.AddPerson(expectedFirstName4, expectedLastName4);
-            Person actualPerson5 = actualPeople.AddPerson(expectedFirstName5, expectedLastName5);
+	    string expectedFirstName5 = "Johan";
+	    string expectedLastName5 = "Jonasson";
 
-            actualPeople.PersonAfterRemove(2);
-            Person[] testPersonArray = actualPeople.FindAll();
+	    // nolla PersonSequencer för att få förutsägelsebara personId
+	    PersonSequencer.reset();
 
-            // Assert
-            Assert.Contains(actualPerson, testPersonArray);
-            Assert.DoesNotContain(actualPerson1, testPersonArray);
+	    // Act
+	    // PersonSequencer.reset();
+	    People actualPeople = new People();
+	    Person actualPerson = actualPeople.AddPerson(expectedFirstName, expectedLastName);
+	    Person actualPerson1 = actualPeople.AddPerson(expectedFirstName1, expectedLastName1);
+	    Person actualPerson2 = actualPeople.AddPerson(expectedFirstName2, expectedLastName2);
+	    Person actualPerson3 = actualPeople.AddPerson(expectedFirstName3, expectedLastName3);
+	    Person actualPerson4 = actualPeople.AddPerson(expectedFirstName4, expectedLastName4);
+	    Person actualPerson5 = actualPeople.AddPerson(expectedFirstName5, expectedLastName5);
 
-            Assert.NotEqual(actualPerson.PersonId, actualPerson1.PersonId);
-            Assert.True(actualPerson.PersonId == 1);
-            Assert.True(actualPerson2.PersonId == 3);
+	    actualPeople.PersonAfterRemove(2);
+	    Person[] testPersonArray = actualPeople.FindAll();
 
-            Assert.Contains(actualPeople.FindById(1).FirstName, "Erik");
-            Assert.Contains(actualPeople.FindById(3).FirstName, "Helen");
-            Assert.Contains(actualPeople.FindById(4).FirstName, "Eva");
-            Assert.Contains(actualPeople.FindById(5).FirstName, "Ulf");
-            Assert.Contains(actualPeople.FindById(6).FirstName, "Johan");
+	    // Assert
+	    Assert.Contains(actualPerson, testPersonArray);
+	    Assert.DoesNotContain(actualPerson1, testPersonArray);
 
-            //Act
-            //PersonSequencer.reset();
-            actualPeople.PersonAfterRemove(4);
-            Person[] testPersonArray1 = actualPeople.FindAll();
+	    Assert.NotEqual(actualPerson.PersonId, actualPerson1.PersonId);
+	    Assert.True(actualPerson.PersonId == 1);
+	    Assert.True(actualPerson2.PersonId == 3);
 
-            //Assert
-            Assert.Contains(actualPerson, testPersonArray1);
-            Assert.DoesNotContain(actualPerson1, testPersonArray1);
-            Assert.Contains(actualPerson2, testPersonArray1);
-            Assert.DoesNotContain(actualPerson3, testPersonArray1);
-            Assert.Contains(actualPerson4, testPersonArray1);
-            Assert.Contains(actualPerson5, testPersonArray1);
-        }
+	    Assert.Contains(actualPeople.FindById(1).FirstName, "Erik");
+	    Assert.Contains(actualPeople.FindById(3).FirstName, "Helen");
+	    Assert.Contains(actualPeople.FindById(4).FirstName, "Eva");
+	    Assert.Contains(actualPeople.FindById(5).FirstName, "Ulf");
+	    Assert.Contains(actualPeople.FindById(6).FirstName, "Johan");
+
+	    //Act
+	    //PersonSequencer.reset();
+	    actualPeople.PersonAfterRemove(4);
+	    Person[] testPersonArray1 = actualPeople.FindAll();
+
+	    //Assert
+	    Assert.Contains(actualPerson, testPersonArray1);
+	    Assert.DoesNotContain(actualPerson1, testPersonArray1);
+	    Assert.Contains(actualPerson2, testPersonArray1);
+	    Assert.DoesNotContain(actualPerson3, testPersonArray1);
+	    Assert.Contains(actualPerson4, testPersonArray1);
+	    Assert.Contains(actualPerson5, testPersonArray1);
+	}
     }
 }
