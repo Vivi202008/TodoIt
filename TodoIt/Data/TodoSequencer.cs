98bdd7a769c3e07be9ca640580974d463761fb0c
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-03 23:08:52 +0200

    TodoSequencer.cs: enbart ändring i mellanslag/space/tabbar etc

--- TodoIt/Data/TodoSequencer.cs
+++ TodoIt/Data/TodoSequencer.cs
@@ -6,16 +6,16 @@ namespace TodoIt.Data
 {
     public class TodoSequencer
     {
-        private static int todoId = 0;
+	private static int todoId = 0;
 
-        public static int nextTodoId()
-        {
-            return ++todoId;
-        }
+	public static int nextTodoId()
+	{
+	    return ++todoId;
+	}
 
-        public static void reset()
-        {
-            todoId = 0;
-        }
+	public static void reset()
+	{
+	    todoId = 0;
+	}
     }
 }
