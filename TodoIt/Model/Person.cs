ad2dd7dc3f331b6213f4f69be2bdb24694404dc3
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-03 22:54:23 +0200

    omskrivning av creator till att använda :this(...

--- TodoIt/Model/Person.cs
+++ TodoIt/Model/Person.cs
@@ -7,58 +7,56 @@ namespace TodoIt.Model
 {
     public class Person
     {
-        static int idCounter = 0;
-        public static int Counter { get { return idCounter; } }
+	static int idCounter = 0;
+	public static int Counter { get { return idCounter; } }
 
-        private readonly int personId;
-        public int PersonId { get { return personId; } }
+	private readonly int personId;
+	public int PersonId { get { return personId; } }
 
-        string firstName;
-        string lastName;//default value stats with is null.
-        public string FirstName
-        {
-            get { return firstName; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Empty or only whitespace is not allowed.");
-                }
-                firstName = value;
-            }
-        }
-        public string LastName
-        {
-            get { return lastName; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Empty or only whitespace is not allowed.");
-                }
+	string firstName;
+	string lastName;//default value stats with is null.
+	public string FirstName
+	{
+	    get { return firstName; }
+	    set
+	    {
+		if (string.IsNullOrEmpty(value))
+		{
+		    throw new ArgumentException("Empty or only whitespace is not allowed.");
+		}
+		firstName = value;
+	    }
+	}
+	public string LastName
+	{
+	    get { return lastName; }
+	    set
+	    {
+		if (string.IsNullOrEmpty(value))
+		{
+		    throw new ArgumentException("Empty or only whitespace is not allowed.");
+		}
 
-                lastName = value;
-            }
-        }
-        public string FullName { get { return $"{firstName} ${lastName}"; } }
+		lastName = value;
+	    }
+	}
+	public string FullName { get { return $"{firstName} {lastName}"; } }
 
-        public Person(string firstName, string lastName)
-        {
-            personId = ++idCounter;
-            FirstName = firstName;
-            LastName = lastName;
-        }
+	public Person(string firstName, string lastName)
+	{
+	    personId = ++idCounter;
+	    FirstName = firstName;  // property kontrollerar att namn inte är NULL
+	    LastName = lastName;    // property kontrollerar att namn inte är NULL
+	}
 
-        public Person(string firstName, string lastName, int id)
-        {
-            personId = id;
-            FirstName = firstName;
-            LastName = lastName;
-        }
+	public Person(string firstName, string lastName, int id):this(firstName, lastName)
+	{
+	    personId = id;
+	}
 
-        public string Details()
-        {
-            return $"personId: {personId}\nName: {FullName}";
-        }
+	public string Details()
+	{
+	    return $"personId: {personId}\nName: {FullName}";
+	}
     }
 }
